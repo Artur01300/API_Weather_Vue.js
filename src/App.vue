99cd9e60950d0faafe35b22b42b636fe98c923c5
Artur01300 <artur.karapetyan@outlook.fr>
2021-06-08 17:04:16 +0200

    commits ajoutés

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,17 @@
   <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
     <main>
       <div class="search-box">
+        <!--v-model="query":  récupère l'inpute-->
+        <!--  @keypress="fetchWeather": permet d'avoir le fonction d'entrer -->
         <input type="text" 
           class="search-bar" 
-          placeholder="Chearch..."
+          placeholder="Recherche..."
           v-model="query"
           @keypress="fetchWeather"
         />
       </div>
 
+      <!-- si les données du weather.main sont manquantes on afficher ce contenue -->
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
           <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
@@ -17,7 +20,7 @@
         </div>
         <div class="weather-box">
           <div class="temp">{{Math.round(weather.main.temp)}}°c</div>
-            <div class="weather">{{weather.weather[0].main}}</div>
+          <div class="weather">{{weather.weather[0].main}}</div>
         </div>
       </div>
     </main>
@@ -31,7 +34,9 @@ export default {
   name: 'App',
   data (){
     return {
+      //j'ajoute la clé proposé par le site de cet API (https://home.openweathermap.org/api_keys)
       api_key: '6eb1e4c44e0f04b72864db80d0f37618',
+       //j'ajoute l'url proposé par le site de cet API (https://openweathermap.org/current), voir "API call"
       url_base: 'http://api.openweathermap.org/data/2.5/',
       query: '',
       weather: {}
@@ -39,7 +44,12 @@ export default {
   },
   methods: {
     fetchWeather(e){
+      /*
+        on vérifi si l'utilisateur a appuié sur le bouton d'entrer pour la recherche des pays
+        (une fois le nom de pays écrit). Si oui alors on exécute le code
+      */
       if(e.key == "Enter"){
+        //dans le fetch on implément l'instuction données par ce site internet: (https://openweathermap.org/current)
         fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
         .then(res => {
           if(res.status === 404){
@@ -51,6 +61,7 @@ export default {
     },
     setResults (results){
       this.weather = results;
+      console.log('results', this.weather)
     },
     dataBuilder(){
       let d = new Date();
