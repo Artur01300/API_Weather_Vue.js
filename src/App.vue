28a0af59aa5971d6f0b87e4e6825286aacf68af6
Artur01300 <artur.karapetyan@outlook.fr>
2021-06-08 11:11:34 +0200

    La date ajoutée

--- src/App.vue
+++ src/App.vue
@@ -13,11 +13,11 @@
       <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
           <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
-          <div class="date">Monday 20 January 2020</div>
+          <div class="date">{{dataBuilder()}}</div>
         </div>
         <div class="weather-box">
-          <div class="temp">9°c</div>
-            <div class="weather">Rain</div>
+          <div class="temp">{{Math.round(weather.main.temp)}}°c</div>
+            <div class="weather">{{weather.weather[0].main}}</div>
         </div>
       </div>
     </main>
@@ -42,14 +42,30 @@ export default {
       if(e.key == "Enter"){
         fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
         .then(res => {
-          console.log('resulta', res);
-          console.log('resulta/main', this.weather);
+          console.log('resulta/', );
           return res.json();
         }).then(this.setResults);
       }
     },
     setResults (results){
       this.weather = results;
+    },
+    dataBuilder(){
+      let d = new Date();
+      let months = [
+        "Janvier", "Février", "Mars", "Avril", "Mai", "Juin", 
+        "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre", 
+      ];
+      let days = [
+        "Dimanche", "Lundi", "Mardi", "Mecredi", "Jeudi", "Vendredi", "Samedi"
+      ];
+
+      let day = days[d.getDay()];
+      let data = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return `${day} ${data} ${month} ${year}`;
     }
   }
 }
